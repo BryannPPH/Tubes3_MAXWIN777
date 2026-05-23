5a40a0d35ec9d67aa3104a6e5b790b83d1108151
BryannPPH <bryanpratama0111@gmail.com>
2026-05-23 18:57:08 +0700

    feat: add type declaration file

---
+++ src/extension/chrome.d.ts
@@ -0,0 +1,66 @@
+interface ChromeRuntimeLastError {
+  message?: string;
+}
+
+interface ChromeMessageSender {
+  tab?: {
+    id?: number;
+  };
+}
+
+interface ChromeEvent<TCallback extends (...args: never[]) => unknown> {
+  addListener(callback: TCallback): void;
+  removeListener(callback: TCallback): void;
+}
+
+interface ChromeRuntimeApi {
+  lastError?: ChromeRuntimeLastError;
+  onMessage: ChromeEvent<
+    (
+      message: unknown,
+      sender: ChromeMessageSender,
+      sendResponse: (response?: unknown) => void,
+    ) => boolean | void
+  >;
+}
+
+interface ChromeStorageChange {
+  oldValue?: unknown;
+  newValue?: unknown;
+}
+
+interface ChromeStorageArea {
+  get(
+    keys: string | string[] | Record<string, unknown> | null,
+    callback: (items: Record<string, unknown>) => void,
+  ): void;
+  set(items: Record<string, unknown>, callback?: () => void): void;
+}
+
+interface ChromeStorageApi {
+  local: ChromeStorageArea;
+  onChanged: ChromeEvent<
+    (changes: Record<string, ChromeStorageChange>, areaName: string) => void
+  >;
+}
+
+interface ChromeTab {
+  id?: number;
+}
+
+interface ChromeTabsApi {
+  query(queryInfo: Record<string, unknown>, callback: (tabs: ChromeTab[]) => void): void;
+  sendMessage(
+    tabId: number,
+    message: unknown,
+    callback?: (response: unknown) => void,
+  ): void;
+}
+
+interface ChromeExtensionApi {
+  runtime: ChromeRuntimeApi;
+  storage: ChromeStorageApi;
+  tabs: ChromeTabsApi;
+}
+
+declare const chrome: ChromeExtensionApi;
